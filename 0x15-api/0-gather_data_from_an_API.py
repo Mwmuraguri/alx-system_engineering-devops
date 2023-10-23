45c907127198bcf99c79f75fbdea0efb1ccb041c
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-10-23 04:42:36 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,19 +6,12 @@ Returns to-do list information for a given employee ID.
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Base URL for the API
+
+def get_todo_list(employee_id):
     base_url = "https://jsonplaceholder.typicode.com/"
-    
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
 
-    employee_id = sys.argv[1]
-
-    # Retrieve user and todos data
-    user_response = requests.get(base_url + "users/{}".format(employee_id))
-    todos_response = requests.get(base_url + "todos", params={"userId": employee_id})
+    user_response = requests.get(f"{base_url}users/{employee_id}")
+    todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
 
     if user_response.status_code != 200:
         sys.exit("User not found")
@@ -29,12 +22,21 @@ if __name__ == "__main__":
     user_data = user_response.json()
     todos_data = todos_response.json()
 
-    # Filter completed tasks
     completed_tasks = [task["title"] for task in todos_data if task["completed"]]
 
-    # Display the results
+    return user_data, completed_tasks
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
+
+    employee_id = sys.argv[1]
+
+    user_data, completed_tasks = get_todo_list(employee_id)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data["name"], len(completed_tasks), len(todos_data)))
+        user_data["name"], len(completed_tasks), len(user_data["todos"])))
 
     for task in completed_tasks:
         print("\t {}".format(task))
