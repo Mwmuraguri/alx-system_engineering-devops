d43a8ee62d479b8802ba2ce74beb10b7b5d07d9d
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-10-23 07:17:52 -0700

    Correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,37 +6,37 @@ Returns to-do list information for a given employee ID.
 import requests
 import sys
 
-# Base URL for the API
-BASE_URL = "https://jsonplaceholder.typicode.com/"
+if __name__ == "__main__":
+    # Base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com/"
 
-# Check if the correct number of arguments is provided
-if len(sys.argv) != 2:
-    sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 2:
+        sys.exit("Usage: {} <employee_id>".format(sys.argv[0]))
 
-employee_id = sys.argv[1]
+    employee_id = sys.argv[1]
 
-# Retrieve user and todos data
-user_response = requests.get(BASE_URL + "users/{}".format(employee_id))
-todos_response = requests.get(BASE_URL + "todos", params={"userId": employee_id})
+    # Retrieve user and todos data
+    user_response = requests.get(base_url + "users/{}".format(employee_id))
+    todos_response = requests.get(base_url +
+        "todos", params={"userId": employee_id})
 
-# Exit if the user or TODO list is not found
-if user_response.status_code != 200:
-    sys.exit("User not found")
+    if user_response.status_code != 200:
+        sys.exit("User not found")
 
-if todos_response.status_code != 200:
-    sys.exit("TODO list not found")
+    if todos_response.status_code != 200:
+        sys.exit("TODO list not found")
 
-user_data = user_response.json()
-todos_data = todos_response.json()
+    user_data = user_response.json()
+    todos_data = todos_response.json()
 
-# Filter completed tasks
-completed_tasks = [task["title"] for task in todos_data if task["completed"]]
+    # Filter completed tasks
+    completed_tasks = [task["title"]
+            for task in todos_data if task["completed"]]
 
-# Display the results
-print("Employee {} is done with tasks({}/{}):".format(
-    user_data["name"], len(completed_tasks), len(todos_data)
-))
+    # Display the results
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data["name"], len(completed_tasks), len(todos_data)))
 
-for task in completed_tasks:
-    print("\t {}".format(task))
-
+    for task in completed_tasks:
+        print("\t {}".format(task))
